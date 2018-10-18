4618cc97f0491ed73872a1751b824f8c208b4b6c
sebeff <sebastien@effilab.com>
2018-10-18 10:29:51 +0200

    sidekiq worker updated + table created

--- app/sidekiq.rb
+++ app/sidekiq.rb
@@ -5,3 +5,6 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis.example.com:7372/12' }
 end
+
+require 'net/http' 
+require "byebug"
--- app/workers/my_worker.rb
+++ app/workers/my_worker.rb
@@ -3,7 +3,19 @@ class MyWorker
   def perform(name, count)
     logger.info "#{name} says the count is #{count}"
   end
+  #api geogouv worker#
+  require 'net/http' 
+  require "byebug"
+  url = URI.parse('https://geo.api.gouv.fr/departements/78/communes?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre')
+  req = Net::HTTP::Get.new(url.to_s)
+  puts url.host 
+  puts url.port
+  res = Net::HTTP.start(url.host, url.port, use_ssl: true) {|http|
+  http.request(req)
+  }puts res.body
 end
+
+
 
 20.times do |index|
   MyWorker.perform_in(index, "Bobby", index)
