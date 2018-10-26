2d97434864476124c3984acd5926755418a1ca3b
sebeff <sebastien@effilab.com>
2018-10-26 16:07:55 +0200

    2510 commit

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ApplicationRecord
   belongs_to :department, optional: true
+  validates_uniqueness_of :code,
   def self.dedupe
     grouped = all.group_by{|city| [city.name,city.code]}
     grouped.values.each do |duplicates|
