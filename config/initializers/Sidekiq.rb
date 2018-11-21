d03bd84e0638a08385470f54441261ee82386809
sebeff <sebastien@effilab.com>
2018-11-21 10:43:01 +0100

    last version of the app

--- config/initializers/Sidekiq.rb
+++ config/initializers/Sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0'}
+  config.redis = { url: 'redis://mylocalredis:6379/0'}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://mylocalredis:6379/0' }
 end
 
 require 'net/http' 
